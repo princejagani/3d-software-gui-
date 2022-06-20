8665c8c9c55b7853c3e7fbe9a395462d045c0e58
princejagani <107679839+princejagani@users.noreply.github.com>
2022-06-20 11:23:22 +0530

    Add files via upload

--- gui.py
+++ gui.py
@@ -1,3 +1,5 @@
+from codecs import BufferedIncrementalDecoder
+from hashlib import new
 from tkinter import *
 from PIL import Image, ImageTk
 from click import command
@@ -5,117 +7,143 @@ from matplotlib.pyplot import fill
 from regex import L
 import gltf_file
 ############################################ all functions ########################################################
-def menu():
-    global status
-    if (status==1):
-        top1frame.pack(side=TOP, fill="x")
-        st12.pack(side=LEFT,padx=10)
-        st11.pack(side=LEFT,padx=10)
-        st9.pack(side=LEFT,padx=10)
-        st8.pack(side=LEFT,padx=10)
-        st7.pack(side=LEFT,padx=10)
-        st6.pack(side=LEFT,padx=10)
-        st5.pack(side=LEFT,padx=10)
-        st4.pack(side=LEFT,padx=10)
-        st3.pack(side=LEFT,padx=10)
-        st2.pack(side=LEFT,padx=10)
-        st1.pack(side=LEFT,padx=10)
-        status=0
-    elif(status==0):
-        top1frame.pack_forget()
-        st12.pack_forget()
-        st11.pack_forget()
-        st10.pack_forget()
-        st9.pack_forget()
-        st8.pack_forget()
-        st7.pack_forget()
-        st6.pack_forget()
-        st5.pack_forget()
-        st4.pack_forget()
-        st3.pack_forget()
-        st2.pack_forget()
-        st1.pack_forget()
-        status=1
+status1=1
+status2=1
+status3=1
+def menu(val):
+    global status1
+    global status2
+    global status3
+    global st
+    if(val==1):
+        if (status1==1):
+            top1frame.pack(side=TOP, fill="x")
+            for i in range(0,len(st)-1):
+                st[i].pack(side=LEFT,padx=10)
+            status1=0
+        elif(status1==0):
+            top1frame.pack_forget()     
+            for i in range(0,len(st)-1):
+                st[i].pack_forget()    
+            status1=1 
+    elif(val==2):
+        if (status2==1):
+            top1frame.pack(side=TOP, fill="x")
+            for i in range(0,len(st)-1):
+                st[i].pack(side=LEFT,padx=10)
+            status2=0
+        elif(status2==0):
+            top1frame.pack_forget()     
+            for i in range(0,len(st)-1):
+                st[i].pack_forget()    
+            status2=1 
+    elif(val==3):
+        if (status3==1):
+            top1frame.pack(side=TOP, fill="x")
+            for i in range(0,len(st)-1):
+                st[i].pack(side=LEFT,padx=10)
+            status3=0
+        elif(status3==0):
+            top1frame.pack_forget()     
+            for i in range(0,len(st)-1):
+                st[i].pack_forget()    
+            status3=1 
+
 
 # def gltf():
 #     app=gltf_file.Slugrace3D()    
 #     app.run()
-
+################################# win starts here #######################################################
 root = Tk()
 width=root.winfo_screenwidth()
 height=root.winfo_screenheight()
 root.geometry("%dx%d" % (width,height))
-
 ################################### left and top  side frame #############################################
 leftframe = Frame(root, bg="#8080ff", borderwidth=6)  
 leftframe.pack(side=LEFT, fill="y")  
+# leftframe.grid(sticky=(S,W,N))
 topframe=Frame(root, bg="#8080ff", borderwidth=14)
 topframe.pack(side=TOP, fill="x")
+# topframe.grid(sticky=(N,W,E))
 centerframe=Frame(root, bg="#fff", borderwidth=6) 
-centerframe.pack(side=BOTTOM, expand=1, fill=BOTH, pady=10)
-# centerframe.grid(column=1,row=2)
-status=1
+centerframe.pack(side=TOP, expand=1, fill=BOTH, pady=10)
+# centerframe.grid(sticky=(N,E,S,W))
 ################################## images ################################################################
-p = Image.open("icons/add-user.png")
-image = p.resize((20,20), Image.ANTIALIAS)
-photo = ImageTk.PhotoImage(image)
-p = Image.open("search.ico")
-image = p.resize((16,16), Image.ANTIALIAS)
-photos = ImageTk.PhotoImage(image)
+p1 = Image.open("icons/MicrosoftTeams-image_1.png")
+image1 = p1.resize((20,20), Image.ANTIALIAS)
+photo1 = ImageTk.PhotoImage(image1)
+p2 = Image.open("icons/MicrosoftTeams-image_2.png")
+image2 = p2.resize((20,20), Image.ANTIALIAS)
+photo2 = ImageTk.PhotoImage(image2)
+p3 = Image.open("icons/MicrosoftTeams-image_3.png")
+image3 = p3.resize((20,20), Image.ANTIALIAS)
+photo3 = ImageTk.PhotoImage(image3)
+p4 = Image.open("icons/MicrosoftTeams-image_4.png")
+image4 = p4.resize((20,20), Image.ANTIALIAS)
+photo4 = ImageTk.PhotoImage(image4)
+p5 = Image.open("icons/MicrosoftTeams-image_5.png")
+image5 = p5.resize((20,20), Image.ANTIALIAS)
+photo5 = ImageTk.PhotoImage(image5)
+p6 = Image.open("icons/search.png")
+image6 = p6.resize((20,20), Image.ANTIALIAS)
+photo6 = ImageTk.PhotoImage(image6)
 ################################## frame button st name ######################################################
 top1frame=Frame(root, bg="#b3b3ff", borderwidth=14)
-st12=Button(top1frame, text="12TH")
-st11=Button(top1frame, text="11TH")
-st10=Button(top1frame, text="10TH")
-st9=Button(top1frame, text="09TH")
-st8=Button(top1frame, text="08TH")
-st7=Button(top1frame, text="07TH")
-st6=Button(top1frame, text="06TH")
-st5=Button(top1frame, text="05TH")
-st4=Button(top1frame, text="04TH")
-st3=Button(top1frame, text="03TH")
-st2=Button(top1frame, text="02TH")
-st1=Button(top1frame, text="01TH")
+st=[]
+s=12
+for i in range(0,13):
+    st.append(Button(top1frame, text=str(s)+"TH"))
+    s=s-1
 #################################################### left frame button ################################
-btn1 = Button(leftframe, text="Submit", fg="skyblue", bg="#8080ff", bd=0, activebackground = "white", image=photo, height="30px",width="30px",relief=RIDGE)  
+fg_icons="skyblue"
+bg_icons="#8080ff"
+abg_icons="white"
+height_icons="30px"
+width_icons="30px"
+btn1 = Button(leftframe, fg=fg_icons, bg=bg_icons, bd=0, activebackground = abg_icons, image=photo1, height=height_icons,width=width_icons,relief=RIDGE)  
 btn1.pack(side=TOP) 
-btn2 = Button(leftframe, text="Submit", fg="skyblue", bg="#8080ff", bd=0, activebackground = "white", image=photo, height="30px",width="30px",relief=RIDGE)  
+btn2 = Button(leftframe, fg=fg_icons, bg=bg_icons, bd=0, activebackground = abg_icons, image=photo2, height=height_icons,width=width_icons,relief=RIDGE)  
 btn2.pack(side=TOP) 
-btn3 = Button(leftframe, text="Submit", fg="skyblue", bg="#8080ff", bd=0, activebackground = "white", image=photo, height="30px",width="30px",relief=RIDGE)  
+btn3 = Button(leftframe, fg=fg_icons, bg=bg_icons, bd=0, activebackground = abg_icons, image=photo3, height=height_icons,width=width_icons,relief=RIDGE)  
 btn3.pack(side=TOP)  
-btn4 = Button(leftframe, text="Submit", fg="skyblue", bg="#8080ff", bd=0, activebackground = "white", image=photo, height="30px",width="30px",relief=RIDGE)  
+btn4 = Button(leftframe, fg=fg_icons, bg=bg_icons, bd=0, activebackground = abg_icons, image=photo4
+, height=height_icons,width=width_icons,relief=RIDGE)  
 btn4.pack(side=TOP)
-btn4 = Button(leftframe, text="Submit", fg="skyblue", bg="#8080ff", bd=0, activebackground = "white", image=photo, height="30px",width="30px",relief=RIDGE)  
+btn4 = Button(leftframe, fg=fg_icons, bg=bg_icons, bd=0, activebackground = abg_icons, image=photo5 , height=height_icons,width=width_icons,relief=RIDGE)  
 btn4.pack(side=TOP)
-btn5 = Button(leftframe, text="Submit", fg="skyblue", bg="#8080ff", bd=0, activebackground = "white", image=photo, height="30px",width="30px",relief=RIDGE)  
+btn5 = Button(leftframe, fg=fg_icons, bg=bg_icons, bd=0, activebackground = abg_icons, image=photo1, height=height_icons,width=width_icons,relief=RIDGE)  
 btn5.pack(side=BOTTOM)  
-###################################################top frame button ###############################
-btn4 = Button(topframe, text="Physics", fg="white", bg="#8080ff", bd=0, activebackground = "gray", relief=RIDGE, padx="10px", command=menu)  
+###################################################top frame button #####################################################################################################
+bg_c_p_b="#8080ff"
+abg_cpb="gray"
+fg_cpb=abg_icons
+btn4 = Button(topframe, text="Physics", fg=fg_cpb, bg=bg_c_p_b, bd=0, activebackground = abg_cpb, relief=RIDGE, padx="10px", command=lambda:menu(1)) 
 btn4.pack(side=LEFT)  
-btn5 = Button(topframe, text="Chemistry", fg="white", bg="#8080ff", bd=0, activebackground = "gray", relief=RIDGE, padx="10px", command=menu)  
+btn5 = Button(topframe, text="Chemistry", fg=fg_cpb, bg=bg_c_p_b, bd=0, activebackground = abg_cpb, relief=RIDGE, padx="10px", command=lambda:menu(2))  
 btn5.pack(side=LEFT)  
-btn6 = Button(topframe, text="Biology", fg="white", bg="#8080ff", bd=0, activebackground = "gray", relief=RIDGE, padx="10px", command=menu)  
+btn6 = Button(topframe, text="Biology", fg=fg_cpb, bg=bg_c_p_b, bd=0, activebackground = abg_cpb, relief=RIDGE, padx="10px", command=lambda:menu(3))
 btn6.pack(side=LEFT)
-btn7 = Button(topframe, fg="white", bg="white", bd=1, activebackground = "gray", relief=RIDGE, height="18px", width="22px", image=photos)  
+btn7 = Button(topframe, fg=abg_icons, bg=fg_cpb, bd=1, activebackground =abg_cpb, relief=RIDGE, height="18px", width="22px", image=photo6)  
 btn7.pack(side=RIGHT)
-e1=Entry(topframe, bg="white",font=0)
+e1=Entry(topframe, bg=abg_icons,font=0)
 e1.pack(side=RIGHT)
-############################################################################ center images ##############################################################
-p1 = Image.open("search.ico")
+############################################################################ center images ###############################################################################
+p1 = Image.open("icons/search.png")
 images = p1.resize((300,150), Image.ANTIALIAS)
 img11 = ImageTk.PhotoImage(images)
 img1=Button(centerframe, text="caption", image=img11,compound=TOP)
-img1.place()
-img1=Button(centerframe, text="caption", image=img11,compound=TOP)
-img1.grid(row=1,column=0)
-img1=Button(centerframe, text="caption", image=img11,compound=TOP)
-img1.grid(row=2,column=0)
-img1=Button(centerframe, text="caption", image=img11,compound=TOP)
-img1.grid(row=3,column=0)
-img1=Button(centerframe, text="caption", image=img11,compound=TOP)
-img1.grid(row=4,column=0)
-img1=Button(centerframe, text="caption", image=img11,compound=TOP)
-img1.grid(row=5,column=0)
-myscrollbar=Scrollbar(centerframe, orient="vertical")
-myscrollbar.pack(side="right",fill="y")
+img1.pack(side=LEFT)
+# img1.place(relx = 0.2, rely = 0.0, anchor ="ne")
+# img1=Button(centerframe, text="caption", image=img11,compound=TOP)
+# img1.grid(row=1,column=0)
+# img1=Button(centerframe, text="caption", image=img11,compound=TOP)
+# img1.grid(row=2,column=0)
+# img1=Button(centerframe, text="caption", image=img11,compound=TOP)
+# img1.grid(row=3,column=0)
+# img1=Button(centerframe, text="caption", image=img11,compound=TOP)
+# img1.grid(row=4,column=0)
+# img1=Button(centerframe, text="caption", image=img11,compound=TOP)
+# img1.grid(row=5,column=0)
+# myscrollbar=Scrollbar(centerframe, orient="vertical")
+# myscrollbar.pack(side="right",fill="y")
 root.mainloop()
